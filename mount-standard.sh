2037b0286255e467db96c359a2452e74cefce60d
kostia <viocost@gmail.com>
2020-01-03 19:04:03 +0000

    debug

---
+++ mount-standard.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+o=defaults.x-mount.mkdir
+o_btrfs=$o,compress=lzo,ssd,noatime
+
+umount -R /mnt
+
+echo Mounting subvolumes
+
+mount -t btrfs -o subvol=root,$o_btrfs LABEL=system /mnt
+mount -t btrfs -o subvol=home,$o_btrfs LABEL=system /mnt/home
+mount -t btrfs -o subvol=snapshots,$o_btrfs LABEL=system /mnt/.snapshots
+
+
+
+
