664967e120189b13bbbc5e0a65025e2e01b87a55
kostia <viocost@gmail.com>
2020-01-04 11:49:07 +0000

    Sed HOOKS modification in mkinitcpio

--- init-grub.sh
+++ init-grub.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+echo checking hooks...
+
+
+input="/etc/mkinitcpio.conf"
+while IFS= read -r line
+do
+	if [[ "$line" == "HOOKS"*"btrfs"* ]]; then
+		echo "BTRFS hook already exists"
+		echo "$line"
+		break
+	
+	elif [[ "$line" == "HOOKS"* ]]; then
+		echo Adding btrfs hook...
+		sed -i '/^HOOKS.*block/s/block/& btrfs/' /etc/mkinitcpio.conf 
+	fi
+done < "$input"
+
+mkinitcpio -p linux
+
+
 pacman -Sy --noconfirm grub efibootmgr
 grub-install --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
+
 
 
--- install.sh
+++ install.sh
@@ -5,4 +5,10 @@ pacstrap /mnt base base-devel btrfs-progs vim sudo intel-ucode wpa-supplicant
 
 genfstab -U /mnt > /mnt/etc/fstab
 
+cp init-grub.sh /mnt/root
+cp stuff.sh /mnt/root
+
+
+arch-chroot /mnt
+
 
