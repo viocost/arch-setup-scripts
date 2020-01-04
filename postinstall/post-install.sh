e52c88c3947c2d6a631b460c45939400b0a63784
kostia <viocost@gmail.com>
2020-01-04 19:48:59 +0500

    postinstall script

---
+++ postinstall/post-install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+
+
+function create_user(){
+	read  -p "Enter username for new user: " username
+	if [[ ${#username} -gt 0 ]]; then
+		echo creating new user "$username"
+		useradd -m -G wheel -s /bin/bash "$username"
+		passwd "$username"
+	fi
+}
+
+function install_packages(){
+	pacman -Sy --noconfirm man git 
+}
+
+function ask_question(){
+	while true; do
+		read -p "$1 Y/n   " yn
+		case $yn in 
+			[Yy]* ) 
+				echo 1
+			       	return
+				;;
+		        [Nn]* ) 
+				echo 0
+			       	return
+				;;
+				
+		esac
+	done
+}
+
+if  [[ $(ask_question "Create user? ")  -gt 0 ]] ; then
+	create_user
+fi
+
+
+if  [[ $(ask_question "Install misc packages?")  -gt 0 ]] ; then
+	install_packages
+fi
+
+
+
+
+
+
+
+
